d5f8b73a96476b0287912481e9f9caddc52202d6
Samuele Illuminati <samuele8@gmail.com>
2021-06-15 23:53:13 +0200

    maps

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,15 +1,16 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
-    // The map, centered at Uluru
+    const venice = { lat: 45.43815683667079, lng: 12.34111728281106 };
+    const murano = { lat: 45.4593620687731, lng: 12.352409296643176 };
+
+
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
+        zoom: 13,
+        center: venice,
     });
-    // The marker, positioned at Uluru
+
     const marker = new google.maps.Marker({
-        position: uluru,
+        position: murano,
         map: map,
+        title: "Murano",
     });
 }
